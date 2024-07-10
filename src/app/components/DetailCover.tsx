6e256e78feb817fb51d41579709a589e16ce8e6e
Chengkun <chengkun05@hotmail.com>
2024-07-11 00:03:29 +0800

    style: style fix

--- src/app/components/DetailCover.tsx
+++ src/app/components/DetailCover.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image'
 
 export default function DetailCover(props: { image: string; title?: string; subtitle?: string }) {
   return (
-    <div className="h-[850px] relative text-white">
+    <div className="h-[calc(100vh-128px)] relative text-white">
       <Image src={props.image} fill alt="" className="object-cover" />
       <div className="absolute bottom-0 left-0 right-0 pb-[150px] px-[11.75%]">
         <h1 className="leading-[92px] text-[66px] font-bold">{props.title}</h1>
